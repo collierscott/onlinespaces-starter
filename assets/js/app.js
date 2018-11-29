6392072cca384c2e44d5d816765a854d8a5703cb
Scott Collier <scott@onlinespaces.com>
2018-11-29 18:51:39 +0100

    Add font awesome and configure so main.scss file is processed

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,9 +6,10 @@
  */
 
 // any CSS you require will output into a single css file (app.css in this case)
+require('../scss/main.scss');
 require('../css/app.css');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // var $ = require('jquery');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+//console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
