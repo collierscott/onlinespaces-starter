b0e454acad5d22a12eb993d6ed91967b2ad96658
Scott Collier <scott@onlinespaces.com>
2018-11-29 21:33:18 +0100

    Allow user to register

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Form\UserRegistrationFormType;
+use App\Security\LoginFormAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -25,9 +30,45 @@ class SecurityController extends AbstractController
     }
 
     /**
+     * @Route("/register", name="form_register")
+     */
+    public function register(Request $request,
+             UserPasswordEncoderInterface $passwordEncoder,
+             GuardAuthenticatorHandler $guardHandler,
+             LoginFormAuthenticator $formAuthenticator)
+    {
+        $form = $this->createForm(UserRegistrationFormType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $user->setPassword($passwordEncoder->encodePassword(
+                $user,
+                $form['plainPassword']->getData()
+            ));
+
+            if (true === $form['agreeTerms']->getData()) {
+                $user->agreeToTerms();
+            }
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+            return $guardHandler->authenticateUserAndHandleSuccess(
+                $user,
+                $request,
+                $formAuthenticator,
+                'main'
+            );
+        }
+        return $this->render('security/register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+
+    /**
      * @Route("/logout", name="form_logout")
      */
     public function logout() {
-        // Should never really be called
+        // This will never be called
     }
 }
