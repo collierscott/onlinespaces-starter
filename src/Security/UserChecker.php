4554f8aed7754865ce848af1f5c49e9f166aa56b
Scott Collier <scott@onlinespaces.com>
2018-11-29 19:42:47 +0100

    Add orm fixtures and faker

---
+++ src/Security/UserChecker.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User;
+use App\Exception\AccountDeletedException;
+use App\Exception\AccountDisabbledException;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class UserChecker implements UserCheckerInterface
+{
+
+    /**
+     * Checks the user account before authentication.
+     *
+     * @param UserInterface $user
+     * @throws AccountDeletedException
+     */
+    public function checkPreAuth(UserInterface $user)
+    {
+        if(!$user instanceof User) {
+            return;
+        }
+
+        // user is deleted, show a generic Account Not Found message.
+        if ($user->getIsDeleted()) {
+            throw new AccountDeletedException('Account Not Found.');
+        }
+    }
+
+    /**
+     * Checks the user account after authentication.
+     *
+     * @param UserInterface $user
+     * @throws AccountDisabbledException
+     */
+    public function checkPostAuth(UserInterface $user)
+    {
+        if (!$user instanceof User) {
+            return;
+        }
+
+        // user account is expired, the user may be notified
+        if ($user->getIsEnabled()) {
+            throw new AccountDisabbledException();
+        }
+    }
+}
