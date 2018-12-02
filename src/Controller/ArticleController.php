23574f1dc8c239b974fa89160c7d1cb7ba0ff9db
Scott Collier <scott@onlinespaces.com>
2018-12-02 21:46:27 +0100

    Admin article controller

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/article{slug}", name="article_show")
+     * @param $slug
+     * @param ArticleRepository $repository
+     * @return Response
+     */
+    public function show($slug, ArticleRepository $repository)
+    {
+        /** @var Article $article */
+        $article = $repository->findOneBy(['slug' => $slug]);
+        if (!$article) {
+            throw $this->createNotFoundException(sprintf('No article for slug "%s"', $slug));
+        }
+        return new Response('show article');
+    }
+}
