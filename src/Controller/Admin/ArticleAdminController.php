620fd439805d270e4915ad18dc7cf865aa2f4a5a
Scott Collier <scott@onlinespaces.com>
2018-12-05 08:08:08 +0100

    Should be able to delete an article

--- src/Controller/Admin/ArticleAdminController.php
+++ src/Controller/Admin/ArticleAdminController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class ArticleAdminController extends AbstractController
 {
@@ -62,15 +63,15 @@ class ArticleAdminController extends AbstractController
      * @param EntityManagerInterface $em
      * @return Response
      */
-    public function edit(Article $article, Request $request, EntityManagerInterface $em)
+    public function edit(Article $article, Request $request, EntityManagerInterface $manager)
     {
         $form = $this->createForm(ArticleFormType::class, $article);
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $em->persist($article);
-            $em->flush();
+            $manager->persist($article);
+            $manager->flush();
 
             $this->addFlash('success', 'Article has been updated.');
 
@@ -82,5 +83,24 @@ class ArticleAdminController extends AbstractController
         return $this->render('admin/article/edit.html.twig', [
             'articleForm' => $form->createView()
         ]);
+    }
+
+    /**
+     * @Route("/admin/article/{id}/delete", name="admin_article_delete", methods={"GET"})
+     *
+     * @param Article $article
+     * @param EntityManagerInterface $manager
+     * @return RedirectResponse
+     */
+    public function delete(Article $article, EntityManagerInterface $manager)
+    {
+        if(!$article) {
+            $this->addFlash('error', 'The article does not exist.');
+            return $this->redirectToRoute('admin_article_list');
+        }
+        $manager->remove($article);
+        $manager->flush();
+        $this->addFlash('success', 'Article has been deleted.');
+        return $this->redirectToRoute('admin_article_list');
     }
 }
