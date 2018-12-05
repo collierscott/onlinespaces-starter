9a900ab2db417b4ac64bd3f38440d31690c56414
Scott Collier <scott@onlinespaces.com>
2018-12-05 15:54:21 +0100

    Trying to get bootstrap javascript to work

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+'use strict';
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -5,11 +6,27 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+//console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
+// require jQuery normally
+const $ = require('jquery');
+
+// create global $ and jQuery variables
+global.$ = global.jQuery = $;
+
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('popper.js');
+require('bootstrap');
+
+// console.log("Hello");
+//
+// $(function() {
+//     console.log( 'Hello with jquery' );
+// });
+//
+// $('#myModal').modal('show');
+
 // any CSS you require will output into a single css file (app.css in this case)
 require('../scss/main.scss');
 require('../css/app.css');
-
-// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-//var $ = require("jquery");
-import $ from 'jquery';
-//console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
