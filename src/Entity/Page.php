5ef9e4a27e1215a2917214369ae310544bcd7cec
Scott Collier <scott@onlinespaces.com>
2018-12-08 11:17:30 +0100

    Page entity

--- src/Entity/Page.php
+++ src/Entity/Page.php
@@ -49,12 +49,12 @@ class Page
     private $socialMetaData;
 
     /**
-     * @var Facebook|null $facebook
+     * @var FacebookPageData|null $facebook
      */
     private $facebook;
 
     /**
-     * @var Twitter|null $twitter
+     * @var TwitterPageData|null $twitter
      */
     private $twitter;
 
@@ -77,6 +77,11 @@ class Page
      * @var string|null $facebookPage
      */
     private $facebookPage;
+
+    public function __construct()
+    {
+        $this->language = "en_US";
+    }
 
     public function getTitle(): ?string
     {
@@ -111,6 +116,240 @@ class Page
     {
         $this->description = $description;
 
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getKeywords(): ?string
+    {
+        return $this->keywords;
+    }
+
+    /**
+     * @param string|null $keywords
+     * @return Page
+     */
+    public function setKeywords(?string $keywords): Page
+    {
+        $this->keywords = $keywords;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param string|null $author
+     * @return Page
+     */
+    public function setAuthor(?string $author): Page
+    {
+        $this->author = $author;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLanguage(): string
+    {
+        return $this->language;
+    }
+
+    /**
+     * @param string $language
+     * @return Page
+     */
+    public function setLanguage(string $language): Page
+    {
+        $this->language = $language;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPublisher(): ?string
+    {
+        return $this->publisher;
+    }
+
+    /**
+     * @param string|null $publisher
+     * @return Page
+     */
+    public function setPublisher(?string $publisher): Page
+    {
+        $this->publisher = $publisher;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getTwitterUsername(): ?string
+    {
+        return $this->twitterUsername;
+    }
+
+    /**
+     * @param string|null $twitterUsername
+     * @return Page
+     */
+    public function setTwitterUsername(?string $twitterUsername): Page
+    {
+        $this->twitterUsername = $twitterUsername;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getGoogleId(): ?string
+    {
+        return $this->googleId;
+    }
+
+    /**
+     * @param string|null $googleId
+     * @return Page
+     */
+    public function setGoogleId(?string $googleId): Page
+    {
+        $this->googleId = $googleId;
+        return $this;
+    }
+
+    /**
+     * @return string[]|null
+     */
+    public function getSocialMetaData(): ?array
+    {
+        return $this->socialMetaData;
+    }
+
+    /**
+     * @param string[]|null $socialMetaData
+     * @return Page
+     */
+    public function setSocialMetaData(?array $socialMetaData): Page
+    {
+        $this->socialMetaData = $socialMetaData;
+        return $this;
+    }
+
+    /**
+     * @return FacebookPageData|null
+     */
+    public function getFacebook(): ?FacebookPageData
+    {
+        return $this->facebook;
+    }
+
+    /**
+     * @param FacebookPageData|null $facebook
+     * @return Page
+     */
+    public function setFacebook(?FacebookPageData $facebook): Page
+    {
+        $this->facebook = $facebook;
+        return $this;
+    }
+
+    /**
+     * @return TwitterPageData|null
+     */
+    public function getTwitter(): ?TwitterPageData
+    {
+        return $this->twitter;
+    }
+
+    /**
+     * @param TwitterPageData|null $twitter
+     * @return Page
+     */
+    public function setTwitter(?TwitterPageData $twitter): Page
+    {
+        $this->twitter = $twitter;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getFacebookAppId(): ?string
+    {
+        return $this->facebookAppId;
+    }
+
+    /**
+     * @param string|null $facebookAppId
+     * @return Page
+     */
+    public function setFacebookAppId(?string $facebookAppId): Page
+    {
+        $this->facebookAppId = $facebookAppId;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getFacebookProfileId(): ?string
+    {
+        return $this->facebookProfileId;
+    }
+
+    /**
+     * @param string|null $facebookProfileId
+     * @return Page
+     */
+    public function setFacebookProfileId(?string $facebookProfileId): Page
+    {
+        $this->facebookProfileId = $facebookProfileId;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getFacebookAdmins(): ?string
+    {
+        return $this->facebookAdmins;
+    }
+
+    /**
+     * @param string|null $facebookAdmins
+     * @return Page
+     */
+    public function setFacebookAdmins(?string $facebookAdmins): Page
+    {
+        $this->facebookAdmins = $facebookAdmins;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getFacebookPage(): ?string
+    {
+        return $this->facebookPage;
+    }
+
+    /**
+     * @param string|null $facebookPage
+     * @return Page
+     */
+    public function setFacebookPage(?string $facebookPage): Page
+    {
+        $this->facebookPage = $facebookPage;
         return $this;
     }
 }
