c1e667ecd0a7e87a2976854ebf4d4f2eaed7df5a
Scott Collier <scott@onlinespaces.com>
2018-12-08 10:16:07 +0100

    Create datafixtures for articles and categories and clean up data fixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -3,13 +3,11 @@
 namespace App\DataFixtures;
 
 use App\Entity\User;
-use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-class UserFixtures extends Fixture
+class UserFixtures extends BaseFixture
 {
-
     private $passwordEncoder;
 
     public function __construct(UserPasswordEncoderInterface $passwordEncoder)
@@ -17,24 +15,49 @@ class UserFixtures extends Fixture
         $this->passwordEncoder = $passwordEncoder;
     }
 
-    public function load(ObjectManager $manager)
+    /**
+     * @param ObjectManager $manager
+     */
+    protected function loadData(ObjectManager $manager)
     {
-        $user = new User();
-        $user->setUsername('scollier');
-        $user->setEmail('scott@onlinespaces.com');
-        $user->setFirstName('Scott');
-        $user->setLastName('Collier');
-        $user->setIsConfirmed(true);
-        $user->setIsEnabled(true);
-        $user->setRoles(['ROLE_USER']);
-        $user->agreeToTerms();
+        $this->createMany(10, 'main_users', function($i) use ($manager) {
+            $user = new User();
+            $user->setEmail(sprintf('user%d@test.com', $i));
+            $user->setUsername(sprintf('user%d', $i));
+            $user->setFirstName($this->faker->firstName);
+            $user->setLastName($this->faker->lastName);
+            $user->setIsEnabled(true);
+            $user->setIsConfirmed(true);
+            $user->setAgreedTermsAt(new \DateTime('now'));
 
-        $user->setPassword($this->passwordEncoder->encodePassword(
-            $user,
-            'Password1'
-        ));
+            $user->setPassword($this->passwordEncoder->encodePassword(
+                $user,
+                'Password1'
+            ));
 
-        $manager->persist($user);
+            return $user;
+        });
+
+        $this->createMany(3, 'admin_users', function($i) {
+            $user = new User();
+            $user->setEmail(sprintf('admin%d@test.com', $i));
+            $user->setUsername(sprintf('admin%d', $i));
+            $user->setFirstName($this->faker->firstName);
+            $user->setLastName($this->faker->lastName);
+            $user->setIsEnabled(true);
+            $user->setIsConfirmed(true);
+            $user->setAgreedTermsAt(new \DateTime('now'));
+
+            $user->setRoles(['ROLE_ADMIN']);
+
+            $user->setPassword($this->passwordEncoder->encodePassword(
+                $user,
+                'Password1'
+            ));
+
+            return $user;
+        });
+
         $manager->flush();
     }
 }
